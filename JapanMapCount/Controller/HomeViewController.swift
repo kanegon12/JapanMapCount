f4eadd92d3d2b7d1ffbf6a19e7fe8bd1fd337e88
kanegon12 <swift.newbie01@gmail.com>
2025-12-27 18:15:51 +0900

    Top画面UIView配置

--- JapanMapCount/Controller/HomeViewController.swift
+++ JapanMapCount/Controller/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  JapanMapCount
 //
 //  Created by Newbie on 2025/12/27.
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
+    @IBOutlet weak var mapView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
