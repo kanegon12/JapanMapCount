32b0718436f721783b87a1b83c8edc1b7786aa55
kanegon12 <swift.newbie01@gmail.com>
2025-12-27 17:36:53 +0900

    Initial commit

---
+++ JapanMapCount/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JapanMapCount
+//
+//  Created by Newbie on 2025/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '18.0'
+
+target 'JapanMapCount' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'RealmSwift'
+
+  # Pods for JapanMapCount
+
+end
