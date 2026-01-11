7ca79f4dd851509c3a6f3d43f5fb07805a468b72
kanegon12 <swift.newbie01@gmail.com>
2026-01-11 22:28:19 +0900

    コード修正

--- JapanMapCount/Controller/JapanMapCountListDetailViewController.swift
+++ JapanMapCount/Controller/JapanMapCountListDetailViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 final class JapanMapCountListDetailViewController: UIViewController {
     @IBOutlet weak var listDetailView: UITableView!
-    @IBAction func sortOrderButton(_ sender: Any) {
-    }
+    @IBOutlet weak var sortOrderButton: UIButton!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // タイトルに県名表示
+        title = prefecture?.displayName
+        configureSortOrderButton()
+        
+    }
+
     private var prefecture: Prefecture?
     
     // prefectureを外部から呼ぶため
@@ -19,11 +26,18 @@ final class JapanMapCountListDetailViewController: UIViewController {
         self.prefecture = prefecture
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // タイトルに県名表示
-        title = prefecture?.displayName
-        
+    private func configureSortOrderButton() {
+        // アイコン
+        sortOrderButton.setTitle(nil, for: .normal)
+        sortOrderButton.setImage(UIImage(systemName: "arrow.up.arrow.down"), for: .normal)
+        // 色
+        sortOrderButton.backgroundColor = .systemBlue
+        sortOrderButton.tintColor = .white
+        // 見た目
+        sortOrderButton.clipsToBounds = true
+        sortOrderButton.layer.cornerRadius = sortOrderButton.frame.width / 2
+
     }
+    
     
 }
--- JapanMapCount/Controller/JapanMapCountTopViewController.swift
+++ JapanMapCount/Controller/JapanMapCountTopViewController.swift
@@ -11,30 +11,20 @@ final class JapanMapCountTopViewController: UIViewController {
     
     @IBOutlet weak var mapView: JapanMapView!
     
-    private var selectedPrefecture: Prefecture?
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
-    }
-    
-    /// 画面遷移が起こる前に自動的に呼ばれるメソッド
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // identifierのIDが"ShowPrefectureDetail"か確認
-        if segue.identifier == "ShowPrefectureDetail",
-           let detailViewController = segue.destination as? JapanMapCountListDetailViewController,
-           let prefectureToPass = selectedPrefecture {
-            // 選ばれた都道府県のデータを遷移先に渡す
-            detailViewController.setPrefecture(prefecture: prefectureToPass)
-        }
     }
 }
 
 extension JapanMapCountTopViewController: JapanMapViewDelegate {
     func japanMapView(_ mapView: JapanMapView, didTap prefecture: Prefecture) {
-        // 画面遷移
-        selectedPrefecture = prefecture
-        performSegue(withIdentifier: "ShowPrefectureDetail", sender: self)
+        // Storyboardを指定して生成
+        let storyboard = UIStoryboard(name: "JapanMapCountListDetail", bundle: nil)
+        guard let listDetailViewController = storyboard.instantiateViewController(withIdentifier: "JapanMapCountListDetail") as? JapanMapCountListDetailViewController else { return }
+        // 値を渡す
+        listDetailViewController.setPrefecture(prefecture: prefecture)
+        // push遷移
+        navigationController?.pushViewController(listDetailViewController, animated: true)
     }
 }
