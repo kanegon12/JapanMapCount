18f17ceab9be37319cd83037c010be4e548a7fd9
kanegon12 <swift.newbie01@gmail.com>
2026-01-15 03:14:10 +0900

    新規登録画面作成

--- JapanMapCount/Controller/JapanMapCountListDetailViewController.swift
+++ JapanMapCount/Controller/JapanMapCountListDetailViewController.swift
@@ -10,13 +10,31 @@ import UIKit
 final class JapanMapCountListDetailViewController: UIViewController {
     @IBOutlet weak var listDetailView: UITableView!
     @IBOutlet weak var sortOrderButton: UIButton!
-    
+    @IBAction func newRegistrationButton(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "JapanMapCountNewRegistration", bundle: nil)
+        guard let navigationViewContloeer = storyboard.instantiateInitialViewController(creator: { coder in
+            return UINavigationController(coder: coder)
+        }) else { return}
+        navigationViewContloeer.modalPresentationStyle = .fullScreen
+        present(navigationViewContloeer, animated: true)
+    }
+    private let prefecture: Prefecture
     private var recordModel: [RecordModel] = []
+    
+    init?(coder: NSCoder, prefecture: Prefecture) {
+        self.prefecture = prefecture
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("int(coder:) has not been errSecUnimplemented")
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // タイトルに県名表示
-        title = prefecture?.displayName
+        title = prefecture.displayName
         configureSortOrderButton()
         
         setTableView()
@@ -45,12 +63,11 @@ final class JapanMapCountListDetailViewController: UIViewController {
         listDetailView.delegate = self
     }
 
-    private var prefecture: Prefecture?
     
-    // prefectureを外部から呼ぶため
-    func setPrefecture(prefecture: Prefecture) {
-        self.prefecture = prefecture
-    }
+//    // prefectureを外部から呼ぶため
+//    func setPrefecture(prefecture: Prefecture) {
+//        self.prefecture = prefecture
+//    }
     
     private func configureSortOrderButton() {
         // アイコン
